0b9a6c5052350a230dbdb1f6711f615639cecf91
Joshua Goring <qualitysoftwareguy@gmail.com>
2026-05-27 00:30:41 -0500

    Fix the imports in src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,11 @@ pub mod model;
 pub mod util;
 
 // Re-export for a more convenient public API
-pub use database::{ManageDatabase, Validate, core::CoreDatabase, user::UserDatabase};
+pub use database::{
+    FromRow, GetById, ManageDatabase, Validate, 
+    core::CoreDatabase, 
+    user::UserDatabase
+};
 pub use environment::resource;
 pub use error::ImpulsePhmError;
 pub use model::{
